352b8d461028cc517e3356625b1fac4fde8c3328
Emre Kose <emrekose5@gmail.com>
2026-05-11 15:08:48 +0100

    + Allowed requests from any origin to prevent extension requests getting blocked

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,6 +1,14 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (request.method === 'OPTIONS') {
+        return response.status(200).end();
+    }
+
     const apiKey = process.env.GEMINI_API_KEY
 
     if (!apiKey) {
