bbb3a394ff47b3170f79bf20bb28d18d44f295ab
Emre Kose <emrekose5@gmail.com>
2026-05-11 14:37:46 +0100

    + Initial commit

---
+++ api/proxy.ts
@@ -0,0 +1,41 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async function handler(request: VercelRequest, response: VercelResponse) {
+    const apiKey = process.env.GEMINI_API_KEY
+
+    if (!apiKey) {
+        return response.status(500).json({ error: 'API key not found.' })
+    }
+
+    try {
+        const aiResponse = await fetch(
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-goog-api-key': apiKey
+                },
+                body: JSON.stringify({
+                    contents: [
+                        {
+                            parts: [
+                                {
+                                    text: JSON.stringify(request.body)
+                                }
+                            ]
+                        }
+                    ]
+                })
+            }
+        )
+
+        const json = await aiResponse.json()
+
+        const responseText = json.candidates?.[0]?.content?.parts?.[0]?.text
+
+        return response.status(aiResponse.status).json(responseText)
+    } catch (error) {
+        return response.status(500).json({ error: 'Failed to obtain API key.' })
+    }
+}
